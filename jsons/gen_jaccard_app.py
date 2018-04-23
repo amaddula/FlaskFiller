ef28e907737d6c82915a46538a4e54d51358f630
jorstern <jorstern@gmail.com>
2018-04-23 03:50:27 -0400

    fixed jaccard

--- jsons/gen_jaccard_app.py
+++ jsons/gen_jaccard_app.py
@@ -12,10 +12,8 @@ ids_names = OrderedDict()
 
 def build_inverted_index(drinks_dict):
     inverted_index = OrderedDict()
-    id_counter = 0
     for drink in drinks_dict:
         drink_name = drink
-        ids_names[id_counter] = drink_name
         drink_ings_list = drinks_dict[drink_name]
         for ingredient in drink_ings_list:
             ing_name = ingredient[0].lower()
@@ -29,14 +27,13 @@ def build_inverted_index(drinks_dict):
 def find_relevant(query,index):
     relevant = set()
     for ing in query:
-        for drink in index[ing]:
+        for drink in index[ing.lower()]:
             relevant.add(drink)
     return relevant
 
 def tf(ing, drink, drinks_dict):
     for tup in drinks_dict[drink]:
-        ingredient = ing.lower()
-        if ingredient == tup[0].lower():
+        if ing == tup[0].lower():
             return tup[1]
     return 0
 
@@ -45,15 +42,25 @@ def gen_jaccard_index_search (query, relevant, drinks_dict):
     results_dict = {}
     drinks_scores = {}
     for drink in relevant:
-        sum_min = 0
-        sum_max = 0
-        for ingredient in drinks_dict[drink]:
-            sum_min += min(query_tf, tf(ingredient[0], drink, drinks_dict))
-            sum_max += max(query_tf, tf(ingredient[0], drink, drinks_dict))
-        for ingredient in query:
-            sum_min += min(query_tf, tf(ingredient[0], drink, drinks_dict))
-            sum_max += max(query_tf, tf(ingredient[0], drink, drinks_dict))
-        similarity = sum_min/sum_max
+        relevant_ings = set(query)
+        sum_min = 0.0
+        sum_max = 0.0
+        
+        for tup in drinks_dict[drink]:
+            ingredient = tup[0].lower()
+            temp_tf = tf(ingredient, drink, drinks_dict)
+            relevant_ings.add(ingredient)
+        for ing in relevant_ings:
+            if ing in query:
+                query_vec = query_tf
+            else:
+                query_vec = 0
+
+            temp_tf = tf(ing, drink, drinks_dict)
+            sum_min += min(query_vec, temp_tf)
+            sum_max += max(query_vec, temp_tf)
+            similarity = sum_min/sum_max
+
         results_dict[drink] = similarity
     return results_dict
 
@@ -65,6 +72,6 @@ def get_results(query):
     results_dict = gen_jaccard_index_search(query, rel, drinks_dict)
     results = []
     sorted_drinks = sorted(results_dict, key=lambda x:results_dict[x], reverse=True)
-    for drink in sorted_drinks[:10]:
+    for drink in sorted_drinks[:50]:
         results.append((results_dict[drink], drink))
     return results
